d3171f4ed72ba3380d97460fce9dc4ef5ca30a06
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-08 17:17:44 +0300

    Task01

---
+++ Tasks/BananaRipenessRange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+       internal class BananaRipenessRange<T> where T : System.Numerics.INumber<T>
+        {
+            public T Minimum { get; set; }
+            public T Maximum { get; set; }
+
+            public BananaRipenessRange(T minimum, T maximum)
+            {
+                if (minimum <= maximum)
+                {
+                    Maximum = maximum;
+                    Minimum = minimum;
+                }
+            }
+
+            public bool IsPerfectBanana(T ripeness)
+            {
+                return ripeness >= Minimum && ripeness <= Maximum;
+            }
+        }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task01
+            BananaRipenessRange<int> bananaRange = new BananaRipenessRange<int>(5, 8);
+            Console.WriteLine(bananaRange.IsPerfectBanana(7));  // Output: True 
+            Console.WriteLine(bananaRange.IsPerfectBanana(3));  // Output: False
+
+            #endregion
+        }
+    }
+}
